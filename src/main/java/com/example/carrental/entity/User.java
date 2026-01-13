b3513fd9fe612a4dc9f7b54ed5dca70e28f15768
Artem <artemgritsenko2007@gmail.com>
2026-01-13 21:35:59 +0200

    Add User entity and Role enum

---
+++ src/main/java/com/example/carrental/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.carrental.entity;
+
+import com.example.carrental.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+@SQLDelete(sql = "UPDATE users SET is_deleted = true WHERE id=?")
+@SQLRestriction("is_deleted=false")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+}
---
+++ src/main/java/com/example/carrental/enums/Role.java
@@ -0,0 +1,7 @@
+package com.example.carrental.enums;
+
+public enum Role {
+    CUSTOMER,
+    MANAGER,
+    ADMIN
+}
