b956521567e98b2e5c7b221656625d15fb650fb3
Artem <artemgritsenko2007@gmail.com>
2026-01-18 14:55:52 +0200

    Add custom user details and service for JWT authentication

---
+++ src/main/java/com/example/carrental/security/CustomUserDetails.java
@@ -0,0 +1,25 @@
+package com.example.carrental.security;
+
+import com.example.carrental.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public record CustomUserDetails(User user) implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+}
--- src/main/java/com/example/carrental/security/jwt/JwtUtil.java
+++ src/main/java/com/example/carrental/security/jwt/JwtUtil.java
@@ -1,4 +1,4 @@
-package com.example.carrental.security.util;
+package com.example.carrental.security.jwt;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
