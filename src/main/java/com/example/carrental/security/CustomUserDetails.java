3b42baadc66d5f3460427ee0ecc33b0e24f8d7fe
Artem <artemgritsenko2007@gmail.com>
2026-01-18 18:13:50 +0200

    Fix CustomUserDetails
    Implement all methods and write getAuthorities

--- src/main/java/com/example/carrental/security/CustomUserDetails.java
+++ src/main/java/com/example/carrental/security/CustomUserDetails.java
@@ -2,15 +2,16 @@ package com.example.carrental.security;
 
 import com.example.carrental.entity.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 
 public record CustomUserDetails(User user) implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
     }
 
     @Override
@@ -21,5 +22,25 @@ public record CustomUserDetails(User user) implements UserDetails {
     @Override
     public String getUsername() {
         return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !user.isDeleted();
     }
 }
